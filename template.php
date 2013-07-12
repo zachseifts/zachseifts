144847beb79e9f2659012c752afb1d20ac654985
Zach Seifts <zach.seifts@gmail.com>
2013-07-12 11:17:31 -0400

    adding the current page title to the breadcrumb

--- template.php
+++ template.php
@@ -10,3 +10,27 @@
 function zachseifts_menu_tree__primary(&$variables) {
   return '<ul class="menu nav nav-pills pull-right">' . $variables['tree'] . '</ul>';
 }
+
+/**
+ * Implements hook_breadcrumb()
+ */
+function zachseifts_breadcrumb($vars) {
+  $breadcrumb = $vars['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+    $breadcrumbs = '<ul class="breadcrumb">';
+
+    $count = count($breadcrumb) - 1;
+    foreach($breadcrumb as $key => $value) {
+      if($count != $key) {
+        $breadcrumbs .= '<li>'.$value.'<span class="divider">/</span></li>';
+      }else{
+        $breadcrumbs .= '<li>'.$value.'</li>';
+      }
+    }
+    $breadcrumbs .= '<li><span class="divider">/</span>' . drupal_get_title() . '</li>';
+    $breadcrumbs .= '</ul>';
+
+    return $breadcrumbs;
+  }
+}
