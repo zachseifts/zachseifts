f8909512fa421f25ab34d8e0a6761ff7a13bf83d
Zach Seifts <seiftsze@appstate.edu>
2013-12-30 17:29:48 +0000

    working onthe theme

--- template.php
+++ template.php
@@ -4,3 +4,10 @@
  * @file
  * template.php
  */
+
+/**
+ * Theme wrapper function for the primary menu links.
+ */
+function zachseifts_menu_tree__primary(&$variables) {
+  return '<ul class="menu nav nav-pills pull-right"">' . $variables['tree'] . '</ul>';
+}
---
+++ templates/system/page.vars.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ * page.vars.php
+ */
+
+/**
+ * Implements hook_preprocess_page().
+ *
+ * @see page.tpl.php
+ */
+function zachseifts_preprocess_page(&$variables) {
+  $variables['content_column_class'] = ' class="col-sm-12"';
+
+  // Primary nav.
+  $variables['primary_nav'] = FALSE;
+  if ($variables['main_menu']) {
+    // Build links.
+    $variables['primary_nav'] = menu_tree(variable_get('menu_main_links_source', 'main-menu'));
+    // Provide default theme wrapper function.
+    $variables['primary_nav']['#theme_wrappers'] = array('menu_tree__primary');
+  }
+}
